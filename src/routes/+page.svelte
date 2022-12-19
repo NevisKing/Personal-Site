8ec987e84168d798cffdc9ae02f9ce17ab3a854a
NevisKing <nevisdontforget@gmail.com>
2022-12-19 12:57:19 +0100

    Changed landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
-<h1>Welcome to the index</h1>
-<a href = "/hello-world">hello world</a>
+<h1>Nevis' personal site</h1>
 
+<p>This is where I'll eventually host whatever information or app examples I want to make public.
+    For now I'm messing around with SvelteKit to understand its ins and outs. If you want to see that
+    heres a 
+
+<a href = "/hello-world">link</a>.
+
+</p>
+
+<style>
+    p {
+        font-family: "Comic-Sans MS";
+        width: 50%;
+        margin: auto;
+        text-align: center;
+    }
+    h1 {
+        margin: auto;
+        width: 50%;
+        text-align: center;
+    }
+</style>
