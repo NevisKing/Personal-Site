7769e08bdaf7b20d75a29e874769810ed85c1eaa
NevisKing <nevisdontforget@gmail.com>
2023-01-04 16:42:49 +0100

    blog order fix

--- front-end/src/routes/blog/+page.server.js
+++ front-end/src/routes/blog/+page.server.js
@@ -3,12 +3,16 @@ import { getBlogPosts } from "$lib/server/database";
 
 export async function load({params}) {
     const data = await getBlogPosts();
+<<<<<<< HEAD
 
     const posts = data.data.sort((a,b) => new Date(b.date) - new Date(a.date));
 
 export async function load({params}) {
     const data = await getBlogPosts();
     const posts = data.data.sort((a,b) => new Date(a.date).getDate() - new Date(b.date).getDate());
+=======
+    const posts = data.data.sort((a,b) => new Date(b.date) - new Date(a.date));
+>>>>>>> a04cb7f (blog order fix)
     return {
         posts: posts
     };
