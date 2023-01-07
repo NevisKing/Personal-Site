dfcfa78fd382381eac3635e091de12103c50498e
NevisKing <nevisdontforget@gmail.com>
2023-01-07 14:20:48 +0100

    fix

--- front-end/src/routes/blog/+page.svelte
+++ front-end/src/routes/blog/+page.svelte
@@ -1,7 +1,12 @@
 <script>
+<<<<<<< HEAD
     import Post from "./post.svelte";
     export let data = {};
 
+=======
+    import Post2022_12_19 from "$lib/blogPosts/2022_12_19.svelte";
+    import Post2023_1_2 from "$lib/blogPosts/2023_1_2.svelte";
+>>>>>>> 06f9108 (palette changes)
 </script>
 
 
@@ -9,9 +14,14 @@
     Nevis' blog
 </h1>
 <div>
+<<<<<<< HEAD
     {#each data.posts as post}
         <Post text={post.text} date={post.date} />
     {/each}
+=======
+    <Post2023_1_2 />
+    <Post2022_12_19 />
+>>>>>>> 06f9108 (palette changes)
 </div>
 
 
